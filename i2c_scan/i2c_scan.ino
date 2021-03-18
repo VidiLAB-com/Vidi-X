3558937342db9b1b5069258a0ef14bbfd37658da
Časopis Vidi <hsomodi@vidi.hr>
2021-03-18 18:23:53 +0100

    I2C scan

---
+++ i2c_scan/i2c_scan.ino
@@ -0,0 +1,49 @@
+/*********
+  Potraga za I2C uređajima
+  spojenim na pinove 32 i 33 
+*********/
+
+#include <Wire.h>
+
+#define I2C_SDA 33
+#define I2C_SCL 32
+
+void setup() {
+  Wire.begin(I2C_SDA, I2C_SCL, 100000);
+  
+  Serial.begin(115200);
+  Serial.println("\nI2C Scanner");
+}
+ 
+void loop() {
+  byte error, address;
+  int nDevices;
+  Serial.println("Scanning...");
+  nDevices = 0;
+  for(address = 1; address < 127; address++ ) {
+    Wire.beginTransmission(address);
+    error = Wire.endTransmission();
+    if (error == 0) {
+      Serial.print("I2C device found at address 0x");
+      if (address<16) {
+        Serial.print("0");
+      }
+      Serial.println(address,HEX);
+      nDevices++;
+    }
+    else if (error==4) {
+      Serial.print("Unknow error at address 0x");
+      if (address<16) {
+        Serial.print("0");
+      }
+      Serial.println(address,HEX);
+    }    
+  }
+  if (nDevices == 0) {
+    Serial.println("No I2C devices found\n");
+  }
+  else {
+    Serial.println("done\n");
+  }
+  delay(5000);          
+}
